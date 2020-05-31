6a4b05d5efc4770e005d26fbfda0c6a5d9598e63
shumpei14 <pein14soccer@gmail.com>
2020-05-31 05:44:14 +0000

    ログイン修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     root_path
   end
   def after_sign_out_path_for(resource)
-    user_session_path 
+    user_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+	def index
+		@users = User.all
+	end
+	def show
+		@user = User.find(prams[:id])
+	end
 end
